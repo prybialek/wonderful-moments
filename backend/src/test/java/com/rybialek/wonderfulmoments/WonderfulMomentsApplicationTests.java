1dd62ec5eb1627aff56f42bc11ec6cadd3d349b8
Pawel Rybialek <pawel.rybialek@gmail.com>
2022-11-29 20:26:37 +0100

    Inital commit

---
+++ backend/src/test/java/com/rybialek/wonderfulmoments/WonderfulMomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rybialek.wonderfulmoments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderfulMomentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
